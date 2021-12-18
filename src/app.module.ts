c214cce891411ed0b20c94cc5fc6c44dfe88c2f1
erratir <mail4entry@gmail.com>
2021-12-19 00:56:10 +0300

    add logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
+import { LoggerMiddleware } from './utils/logger.middleware';
 import { HealthController } from './health/health.controller';
 
 @Module({
@@ -8,4 +10,8 @@ import { HealthController } from './health/health.controller';
   controllers: [AppController, HealthController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from "@nestjs/common";
 
 @Injectable()
 export class AppService {
+  logger: Logger
+
+  constructor() {
+    this.logger = new Logger()
+  }
   getHello(): string {
+    this.logger.log('test log - getHello !');
+    this.logger.warn('test warn - getHello !');
+    this.logger.error('test error - getHello !');
     return 'Hello World!';
   }
 }

1e1c05eff0d819a66f84e05020c2b7c75206b270
Aleksey Kobzev <akobzev@medsoft.su>
2021-12-20 10:45:27 +0300

    use Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import {
   FastifyAdapter,
   NestFastifyApplication,
@@ -10,6 +11,16 @@ async function bootstrap() {
     AppModule,
     new FastifyAdapter({ logger: true }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Nest.js (Fastify) app API')
+    .setDescription('Nest.js  (Fastify, Swagger) app  API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3001);
 }
 bootstrap();
